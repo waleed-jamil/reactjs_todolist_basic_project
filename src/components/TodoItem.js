d5984c9615292d5fa6712f2d177dfe23e172ca0f
Waleed Jamil <urzwalid@gmail.com>
2020-08-10 16:32:35 +0800

    adding components - Todos, TodoItem

---
+++ src/components/TodoItem.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class TodoItem extends Component {
+    render() {
+        return (
+            <div style = {{ backgroundColor: '#f4f4f4'}}>
+                <p>{ this.props.todo.title }</p>
+            </div>
+        )
+    }
+} 
+
+
+//PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+
+export default TodoItem;
